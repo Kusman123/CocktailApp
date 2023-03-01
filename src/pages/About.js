fef1f4694f967e1f86d4c8874826e2b54b43280d
Kusman123 <usmanahmadkhan93@gmail.com>
2023-03-01 12:24:07 +0530

    first commit

---
+++ src/pages/About.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+      <p>
+        Hello this page is an one stop solution for all the cocktail recipies.
+        So please try to make them at your home and enjoy it with your loved
+        once. Also please share your valueable feedback to us by sending your
+        email at <b>usmanahmadkhan93@gmail.com</b>. If you want us to upload
+        some new recipies, please let us know about that too.
+      </p>
+    </section>
+  );
+};
+
+export default About;
